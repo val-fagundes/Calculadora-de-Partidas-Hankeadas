823c4e5096c443b3e513839a87c82246bf388886
val-fagundes <fagundes.tec35@gmail.com>
2023-12-21 03:14:55 -0300

    first commit

--- index.js
+++ index.js
@@ -5,27 +5,20 @@ function calcularNivelRanqueadas(vitorias, derrotas) {
   const saldo = vitorias - derrotas;
   let nivel;
 
-  switch (true) {
-    case vitorias < 10:
-      nivel = "Ferro";
-      break;
-    case vitorias >= 11 && vitorias <= 20:
-      nivel = "Bronze";
-      break;
-    case vitorias >= 21 && vitorias <= 50:
-      nivel = "Prata";
-      break;
-    case vitorias >= 51 && vitorias <= 80:
-      nivel = "Ouro";
-      break;
-    case vitorias >= 81 && vitorias <= 90:
-      nivel = "Diamante";
-      break;
-    case vitorias >= 91 && vitorias <= 100:
-      nivel = "Lendário";
-      break;
-    default:
-      nivel = "Imortal";
+  if (vitorias < 10) {
+    nivel = "Ferro";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
   }
 
   return { saldo, nivel };
